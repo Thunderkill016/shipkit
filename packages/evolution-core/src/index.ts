a2abf61ea5f5c5fb40c6854bc96da86e5c91ea1f
Thunderkill <66909862+Thunderkill016@users.noreply.github.com>
2026-07-23 10:25:21 +0700

    feat(evolution): export repository and evidence APIs

--- packages/evolution-core/src/index.ts
+++ packages/evolution-core/src/index.ts
@@ -2,3 +2,5 @@ export * from "./types.js";
 export * from "./policy.js";
 export * from "./state-machine.js";
 export * from "./persistence.js";
+export * from "./repository.js";
+export * from "./evidence.js";
